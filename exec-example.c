5b2059da874c878cf7c86426bdb51d243192ab40
Ishan-Bashyal <ishanbashyal11@gmail.com>
2026-07-14 13:35:05 +0545

    Exec() example
    Exec() example using execl to replace process image

---
+++ exec-example.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main() {
+    printf("Before exec\n");
+
+    execl("/bin/ls", "ls", "-l", NULL);
+
+    printf("After exec\n");
+
+    return 0;
+}
+
+
